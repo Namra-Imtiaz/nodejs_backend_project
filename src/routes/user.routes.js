24c3735d686b33bc615cb12b2d9f85c8c30c51ce
Namra-Imtiaz <namraimtiaz04@gmail.com>
2025-09-06 10:39:35 -0700

    adding routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import {registerController,loginController, logoutController } from "../controllers/user.controller.js";
+import {registerController,
+        loginController, 
+        logoutController, 
+        changeCurrentPasswordController, 
+        getCurrentUserController, 
+        updateUserController, 
+        updateAvatar, 
+        updateCoverImage, 
+        getUserChannelProfile, 
+        getwatchHistory 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { refreshAccessTokenController } from "../controllers/user.controller.js";
-
 const router=Router();  //jis tarah express ko initialize kartai ho app variable mai isi tarah router ko bhi kartai hain
 router.route('/register').post(
     upload.fields([
@@ -25,6 +34,20 @@ router.route('/login').post(loginController);
 router.route("/logout").post(verifyJWT,logoutController);
 
 router.route("/refresh-token").post(refreshAccessTokenController);
+
+router.route("/change-password").post(verifyJWT,changeCurrentPasswordController);
+
+router.route("/current-user").get(verifyJWT,getCurrentUserController);
+
+router.route("/update-user").patch(verifyJWT,updateUserController);
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar);
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+
+router.route("/history").get(verifyJWT,getwatchHistory);
 
 
 
